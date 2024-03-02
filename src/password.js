a155a8f0062dc5d3d729f2c3f964c9d2da6bc9dd
dvlpr2003 <gayathrigaya698@gmail.com>
2024-03-02 22:48:38 +0530

    f*ck

--- src/password.js
+++ src/password.js
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import "./password.css";
 export default function PasswordBody(){
     return(
@@ -10,15 +11,22 @@ export default function PasswordBody(){
 }
 
 function PasswordEntry(){
+    const InputElement = useRef(null)
+    useEffect(function(){
+        InputElement.current.focus()
+    })
     return(
         <div className="password-container">
             <div id="inform-element">
             <label for="web-name">Website name</label>
-            <input placeholder="Example: Google,Microsoft,Facebook" id="web-name"/>
+            <input placeholder="Example: Google,Microsoft,Facebook" id="web-name" ref={InputElement}/>
             <label for="username">Username</label>
-            <input placeholder="" id="username"/>
+            <input placeholder="" id="username" />
             <GeneratePassword/>
-
+            <div id="btn-1">
+                <button>Clear All</button>
+                <button>Confirm</button>
+            </div>
             </div>
         </div>
     )
