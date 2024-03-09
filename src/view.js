4d00f398215ef28ae205e953bd099546735b47c9
dvlpr2003 <gayathrigaya698@gmail.com>
2024-03-09 22:24:51 +0530

    password

--- src/view.js
+++ src/view.js
@@ -3,25 +3,16 @@ export default function ViewBox({True,setTrue,ViewPage,setViewPage,Final}){
         setTrue((e)=>!e)
         setViewPage()
     }
+    console.log(Final.at(ViewPage-1))
     return (
         <div id="view-box" style={True?{display:"none"}:{display:"block"} }>
             <div id="view-box-2">
             <button id="x-btn" onClick={CancelBtn}>&#9747;</button>
-            {
-                Final.map((e)=><BtnElements web = {e.website_name} user={e.User_name} password ={e.password}/>)
-            }
-            
+            <span>Website Name : {Final.at(ViewPage-1)["website_name"]}</span>
+                <span>User Name : {Final.at(ViewPage-1)["User_name"]}</span>
+                <span>Password : {Final.at(ViewPage-1)["password"]} </span>
 
             </div>
         </div>
-    )
-}
-function BtnElements({web,user,password}){
-    return(
-        <>
-                <span>Website Name : {web}</span>
-                <span>User Name : {user}</span>
-                <span>Password : {password}</span>
-        </>
     )
 }
